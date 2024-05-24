59c59b89c8b7cd3439ca3a464fbc0131ed6c8ef0
Av <yungvade1@gmail.com>
2024-05-24 14:02:21 -0700

    api

--- client/src/helpers/Constants.ts
+++ client/src/helpers/Constants.ts
@@ -1,1 +1,4 @@
-export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:5001/api";
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "https://url-shortener-apii.onrender.com";
+
+//http://localhost:5001/api
+
